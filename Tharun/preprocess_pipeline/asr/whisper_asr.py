485fcd198db15b1a8d0336d4b69cdd767c1b7524
Tharun-Rv <113426855+Tharun-Rv@users.noreply.github.com>
2025-12-30 20:05:14 +0530

    Delete Tharun/preprocess_pipeline directory

--- Tharun/preprocess_pipeline/asr/whisper_asr.py
+++
@@ -1,6 +0,0 @@
-from faster_whisper import WhisperModel
-
-def transcribe_whisper(audio_path):
-    model = WhisperModel("base", device="cpu")
-    segments, _ = model.transcribe(audio_path)
-    return " ".join(seg.text.strip() for seg in segments)
--- Tharun/preprocess_pipeline/audio_pipeline/noise_reduction.py
+++
@@ -1,4 +0,0 @@
-import noisereduce as nr
-
-def reduce_noise(audio, sr):
-    return nr.reduce_noise(y=audio, sr=sr)
