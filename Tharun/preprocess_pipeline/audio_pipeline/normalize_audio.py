cc6cce65a02060b9800ddd4a25b7e35ae72fa019
Tharun-Rv <113426855+Tharun-Rv@users.noreply.github.com>
2025-12-30 19:32:28 +0530

    Delete Tharun/preprocess_pipeline directory

--- Tharun/preprocess_pipeline/audio_pipeline/normalize_audio.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-
-def normalize_audio(audio):
-    return audio / max(abs(audio))
--- Tharun/preprocess_pipeline/audio_pipeline/preprocess_signal.py
+++
@@ -1,7 +0,0 @@
-from .noise_reduction import reduce_noise
-from .normalize_audio import normalize_audio
-
-def preprocess_signal(audio, sr):
-    audio = reduce_noise(audio, sr)
-    audio = normalize_audio(audio)
-    return audio
